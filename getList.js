9636cc10d48369444995441202b97e37dca75858
Khang Hoang <hoangtrieukhang@gmail.com>
2016-07-02 15:55:07 +0700

    fix about REMOTE_URL

--- getList.js
+++ getList.js
@@ -1,9 +1,14 @@
 const Promise = require('bluebird');
 const crawlerjs = require('crawler-js');
 const _ = require('lodash');
-const config = require('./env');
 
-const urlRemote = process.ENV.ENDPOINT_URL || config.ENDPOINT_URL;
+let urlRemote = '';
+if (process.ENV.ENDPOINT_URL) {
+  urlRemote = process.ENV.ENDPOINT_URL;
+} esle {
+  const config = require('./env');
+  urlRemote = config.ENDPOINT_URL;
+}
 
 module.exports = function() {
   return new Promise((resolve, reject) => {
