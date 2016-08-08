bc46511fcbf863810cbc2de6c68223a06daaefd0
Khang Hoang <hoangtrieukhang@gmail.com>
2016-08-09 00:06:38 +0700

    add reds client

---
+++ redis.js
@@ -0,0 +1,23 @@
+'use stricts';
+
+const bluebird = require('bluebird');
+const redis = require('redis');
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL || null
+});
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+module.exports = {
+  saveMovieDataWithUrl: function(url, data) {
+    const strData = JSON.stringify(data);
+    return client.setAsync(url, strData);
+  },
+
+  getMovieWithUrl: function(url) {
+    return client.getAsync(url);
+  }
+}
